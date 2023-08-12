893ae87938d6bfdf0286f1103e588af76bbe576a
hisaojgf <hisaojgf@gmail.com>
2023-08-12 20:41:35 +0200

    Updated about-me description, bday :)

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger'),
-          menu = document.querySelector('.menu'),
-          menuLink = menu.querySelectorAll('.menu__link'),
-          socialText = document.querySelector('.header__text'),
-          socialLinks = document.querySelectorAll('.header__social');
+window.addEventListener("DOMContentLoaded", () => {
+	const hamburger = document.querySelector(".hamburger"),
+		menu = document.querySelector(".menu"),
+		menuLink = menu.querySelectorAll(".menu__link"),
+		socialText = document.querySelector(".header__text"),
+		socialLinks = document.querySelectorAll(".header__social");
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
-    menuLink.forEach(link => {
-        link.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        });
-    });
-    socialText.addEventListener('click', () => {
-        socialText.classList.toggle('header__text_active');
-        socialLinks.forEach(link => {
-            link.classList.toggle('header__social_active');
-        });
-    });
+	hamburger.addEventListener("click", () => {
+		hamburger.classList.toggle("hamburger_active");
+		menu.classList.toggle("menu_active");
+	});
 
-    const percentage = document.querySelectorAll('.skills__correlation-text'),
-          showBar = document.querySelectorAll('.skills__percentage-showbar');
+	menuLink.forEach(link => {
+		link.addEventListener("click", () => {
+			hamburger.classList.toggle("hamburger_active");
+			menu.classList.toggle("menu_active");
+		});
+	});
 
-    percentage.forEach((item, i) => {
-        showBar[i].style.width = item.innerHTML;
-    });
+	socialText.addEventListener("click", () => {
+		socialText.classList.toggle("header__text_active");
+		socialLinks.forEach(link => {
+			link.classList.toggle("header__social_active");
+		});
+	});
+
+	const percentage = document.querySelectorAll(".skills__correlation-text"),
+		showBar = document.querySelectorAll(".skills__percentage-showbar");
+
+	percentage.forEach((item, i) => {
+		showBar[i].style.width = item.textContent;
+	});
 });
